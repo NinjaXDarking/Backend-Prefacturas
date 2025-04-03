9e222532410efe12b27c184fc696680d74d1f07b
RODRIGUEZ LORIA EMMANUEL JAVIER <110081353+NinjaXDarking@users.noreply.github.com>
2025-04-03 01:11:08 -0600

    Backend V3.4
    Prefacturas Avance, intento de insertar prefacturas

--- src/models/prefacture.js
+++ src/models/prefacture.js
@@ -1,24 +1,51 @@
+const sql = require('mssql');
 const connection = require('../configuration/db');
 
 const insertPrefactures = async (DB_USER, DB_PASS, DB_SERVER, DB_DATABASE, CodCliente, NombreCliente, Subtotal, Iva, Total, ListProduct ) => {
     let pool;
     try {
-        //pool = await connection.getConnection(DB_USER, DB_PASS, DB_SERVER, DB_DATABASE);
+        pool = await connection.getConnection(DB_USER, DB_PASS, DB_SERVER, DB_DATABASE);
         
+        const productTable = new sql.Table('ListaProductosType'); // Debe coincidir con el TYPE en SQL Server
+            productTable.columns.add('Codigo', sql.Int);
+            productTable.columns.add('Descripcion', sql.VarChar(250));
+            productTable.columns.add('Cantidad', sql.Float);
+            productTable.columns.add('Precio', sql.Float);
+            productTable.columns.add('Porcentaje', sql.Float);
+
+        ListProduct.forEach(product => {
+            productTable.rows.add(
+                parseInt(product.Codigo), 
+                product.Descripcion, 
+                parseFloat(product.Existencia), 
+                parseFloat(product.PrecioA), 
+                parseFloat(product.porcentaje)
+            );
+        });
         
-        //const result = await pool.request().query('Exec dbo.App_BusquedaProductos');
+        const result = await pool.request()
+            .input('CodCliente', sql.Int, parseInt(CodCliente))
+            .input('NombreCliente', sql.VarChar(250), NombreCliente)
+            .input('SubTotal', sql.Float, parseFloat(Subtotal))
+            .input('TotalImpuesto', sql.Float, parseFloat(Iva))
+            .input('Total', sql.Float, parseFloat(Total))
+            .input('ListaProductos', productTable)  // Pasar el TVP como parámetro
+            .execute('APP_InsertPrefactura');
         
-        const result = {
-            success: true,
-            message: "Datos obtenidos correctamente",
-            NombreCliente: NombreCliente,
-            Subtotal: Subtotal,
-            Iva: Iva,
-            Total: Total,
-            ListProduct: ListProduct
-        };
-        console.log(result);
-        return result;
+        // const result = {
+        //     success: true,
+        //     message: "Datos obtenidos correctamente",
+        //     CodCliente: CodCliente,
+        //     NombreCliente: NombreCliente,
+        //     Subtotal: Subtotal,
+        //     Iva: Iva,
+        //     Total: Total,
+        //     ListProduct: ListProduct
+        // };
+
+        console.log('Prefactura creada exitosamente:', result);
+        return { success: true, message: 'Prefactura creada exitosamente', data: result.recordset };
+
     } catch (error) {
         throw new Error(error);
     } finally {
